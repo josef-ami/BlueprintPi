59bf83d9bef35b16781e2315176c0ab0c26ea7eb
josef-ami <syedsubehsadiksholok@gmail.com>
2026-09-19 14:27:33 +0600

    changed data sent format to left, front, right

--- openRound.py
+++ openRound.py
@@ -77,7 +77,7 @@ def _u16(mm):
 
 
 def pack_frame(front_mm, left_mm, right_mm):
-    payload = struct.pack("<HHH", _u16(front_mm), _u16(left_mm), _u16(right_mm))
+    payload = struct.pack("<HHH", _u16(left_mm), _u16(front_mm), _u16(right_mm))
     xor = 0
     for b in payload:
         xor ^= b
